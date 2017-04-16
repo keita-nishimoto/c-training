e0fa37df6b9b58e9f98a307630d27f9462fb945f
KeitaNishimoto <k.mail.09070876640@icloud.com>
2017-04-17 05:34:02 +0900

    modify プロトタイプ宣言の方法を追加

--- function/main.c
+++ function/main.c
@@ -8,6 +8,21 @@
 
 #include <stdio.h>
 
+// プロトタイプ宣言
+float getGreaterNumber(float a, float b);
+void sayhi(void);
+
+int main(void) {
+    float result;
+    result = getGreaterNumber(2.3, 5.2);
+
+    printf("%f\n", result);
+
+    sayhi();
+
+    return 0;
+}
+
 // 関数定義
 float getGreaterNumber(float a, float b) {
     if (a >= b) {
@@ -17,10 +32,6 @@ float getGreaterNumber(float a, float b) {
     }
 }
 
-int main(void) {
-    float result;
-    result = getGreaterNumber(2.3, 5.2);
-
-    printf("%f\n", result);
-    return 0;
+void sayhi(void) {
+    printf("hi!\n");
 }
